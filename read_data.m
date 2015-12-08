852f013ec739ecd128da02388fbe6db763d25bad
Meg McCauley <megan@mccauley.net>
2015-12-08 10:06:04 -0500

    ADD: find_freq.m function, EDIT: read_data.m

--- read_data.m
+++ read_data.m
@@ -1,9 +1,14 @@
 %read_data.m
 
-f1 = fopen('recv.dat','r');
+%rx_samples_to_file --freq 1e6 --rate 0.25e6 --file file_name.dat --type float
+figure;
+f1 = fopen('recv.dat','r');      %make sure to change the file name when you run in cmd
 
 x = fread(f1, 'float32');
 x_real = x(1:2:end);
 x_imag = x(2:2:end);
+x_complex = x_real+1i*x_imag;
 plot(x_real(100:end));
+res = find_freq(x_complex);
+disp(res)
 fclose(f1);
